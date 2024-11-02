9f48aeb3a2f9b37095c8cb76e3ad3cc1b8e246e7
Andrew Chan <chan.andrew1995@gmail.com>
2024-11-01 20:13:30 -0400

    added a page with a grid

---
+++ BorderRadiusPreviewer/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="container">
+      <div className="top-left corners">top left</div>
+      <div className="top-right corners">top right</div>
+      <div className="center"></div>
+      <div className="bottom-left corners">bottom left</div>
+      <div className="bottom-right corners">bottom right</div>
+    </div>
+  )
+}
+
+export default App
